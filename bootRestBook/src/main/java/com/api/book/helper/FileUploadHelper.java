58c079128c669d2d4e0bf91717125f88026e91f1
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-04-23 17:11:08 +0530

    image uploading and url getting successfully

--- bootRestBook/src/main/java/com/api/book/helper/FileUploadHelper.java
+++ bootRestBook/src/main/java/com/api/book/helper/FileUploadHelper.java
@@ -1,23 +1,42 @@
 package com.api.book.helper;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-	public final String UPLOAD_DIR = "C:\\Users\\Gaurang Dalvadi\\Desktop\\Spring_Boot\\bootRestBook\\src\\main\\resources\\static\\image";
+//	public final String UPLOAD_DIR = "C:\\Users\\Gaurang Dalvadi\\Desktop\\Spring_Boot\\bootRestBook\\src\\main\\resources\\static\\image";
+	public final String UPLOAD_DIR = new ClassPathResource("static/image/").getFile().getAbsolutePath();
 	
-	public boolean uploadFile(MultipartFile file) {
+	public FileUploadHelper() throws IOException
+	{
+		
+	}
+	public boolean uploadFile(MultipartFile multipartFile) {
 		boolean f = false;
 		
 		try {
-			InputStream inputStream = file.getInputStream();
-			byte data[] = new byte[inputStream.available()];
+//			InputStream inputStream = file.getInputStream();
+//			byte data[] = new byte[inputStream.available()];
+//			inputStream.read(data);
+//			
+//			// write
+//			FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+file.getOriginalFilename());
+//			fos.write(data);
+//			
+//			fos.flush();
+//			fos.close();
 			
-			inputStream.read(data);
+			Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+			f = true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
